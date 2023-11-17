8ea362d2d1dfab4b983ef8af23784566e325b515
MartinHolts <martinholtsmeier@gmail.com>
2023-11-17 11:59:54 +0200

    Added Incrementing app.

---
+++ increment-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Increment App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
