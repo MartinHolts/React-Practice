30be64c25475bed9e5d0fdd7efc3ae7ab67f7b5f
MartinHolts <martinholtsmeier@gmail.com>
2023-11-17 11:46:58 +0200

    Made incrementing app

---
+++ react-counter-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
